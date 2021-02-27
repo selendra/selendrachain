a0c0f7a527372a8f7e6ecc4063ab09ea40126d43
laynath242@gmail.com <laynath242@gmail.com>
2021-02-27 09:08:16 +0700

    Change a few fee/weight parameters

--- runtime/indracore/src/lib.rs
+++ runtime/indracore/src/lib.rs
@@ -42,10 +42,10 @@ use pallet_session::historical as session_historical;
 use pallet_transaction_payment::{FeeDetails, RuntimeDispatchInfo};
 use parity_scale_codec::{Decode, Encode};
 use primitives::v1::{
-    AccountId, AccountIndex, Balance, BlockNumber, CandidateEvent,
-    CommittedCandidateReceipt, CoreState, GroupRotationInfo, Hash, Id, InboundDownwardMessage,
-    InboundHrmpMessage, Moment, Nonce, OccupiedCoreAssumption, PersistedValidationData,
-    SessionInfo, Signature, ValidationCode, ValidatorId, ValidatorIndex,
+    AccountId, AccountIndex, Balance, BlockNumber, CandidateEvent, CommittedCandidateReceipt,
+    CoreState, GroupRotationInfo, Hash, Id, InboundDownwardMessage, InboundHrmpMessage, Moment,
+    Nonce, OccupiedCoreAssumption, PersistedValidationData, SessionInfo, Signature, ValidationCode,
+    ValidatorId, ValidatorIndex,
 };
 use sp_core::u32_trait::{_1, _2, _3, _4, _5};
 use sp_core::OpaqueMetadata;
@@ -1031,10 +1031,10 @@ impl pallet_sudo::Config for Runtime {
 
 pub struct CustomOnRuntimeUpgrade;
 impl frame_support::traits::OnRuntimeUpgrade for CustomOnRuntimeUpgrade {
-	fn on_runtime_upgrade() -> frame_support::weights::Weight {
-		0
-	}
-} 
+    fn on_runtime_upgrade() -> frame_support::weights::Weight {
+        0
+    }
+}
 
 construct_runtime! {
     pub enum Runtime where
@@ -1443,5 +1443,64 @@ sp_api::impl_runtime_apis! {
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
             Ok(batches)
         }
+    }
+}
+
+#[cfg(test)]
+mod test_fees {
+    use super::*;
+    use frame_support::storage::StorageValue;
+    use frame_support::weights::GetDispatchInfo;
+    use frame_support::weights::WeightToFeePolynomial;
+    use pallet_transaction_payment::Multiplier;
+    use parity_scale_codec::Encode;
+    use separator::Separatable;
+    use sp_runtime::FixedPointNumber;
+
+    #[test]
+    #[ignore]
+    fn block_cost() {
+        let max_block_weight = BlockWeights::get().max_block;
+        let raw_fee = WeightToFee::calc(&max_block_weight);
+
+        println!(
+            "Full Block weight == {} // WeightToFee(full_block) == {} plank",
+            max_block_weight,
+            raw_fee.separated_string(),
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn transfer_cost_min_multiplier() {
+        let min_multiplier = runtime_common::MinimumMultiplier::get();
+        let call = <pallet_balances::Call<Runtime>>::transfer_keep_alive(
+            Default::default(),
+            Default::default(),
+        );
+        let info = call.get_dispatch_info();
+        // convert to outer call.
+        let call = Call::Balances(call);
+        let len = call.using_encoded(|e| e.len()) as u32;
+
+        let mut ext = sp_io::TestExternalities::new_empty();
+        let mut test_with_multiplier = |m| {
+            ext.execute_with(|| {
+                pallet_transaction_payment::NextFeeMultiplier::put(m);
+                let fee = TransactionPayment::compute_fee(len, &info, 0);
+                println!(
+                    "weight = {:?} // multiplier = {:?} // full transfer fee = {:?}",
+                    info.weight.separated_string(),
+                    pallet_transaction_payment::NextFeeMultiplier::get(),
+                    fee.separated_string(),
+                );
+            });
+        };
+
+        test_with_multiplier(min_multiplier);
+        test_with_multiplier(Multiplier::saturating_from_rational(1, 1u128));
+        test_with_multiplier(Multiplier::saturating_from_rational(1, 1_000u128));
+        test_with_multiplier(Multiplier::saturating_from_rational(1, 1_000_000u128));
+        test_with_multiplier(Multiplier::saturating_from_rational(1, 1_000_000_000u128));
     }
 }
