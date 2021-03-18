6a90b4b7818a48e400fe8bd371613f9f61e7d3c0
laynath242@gmail.com <laynath242@gmail.com>
2021-03-18 09:41:52 +0700

    Fix wrong deposit amount in council voters

--- runtime/indracore/src/lib.rs
+++ runtime/indracore/src/lib.rs
@@ -1087,13 +1087,6 @@ impl pallet_sudo::Config for Runtime {
     type Call = Call;
 }
 
-pub struct CustomOnRuntimeUpgrade;
-impl frame_support::traits::OnRuntimeUpgrade for CustomOnRuntimeUpgrade {
-    fn on_runtime_upgrade() -> frame_support::weights::Weight {
-        0
-    }
-}
-
 construct_runtime! {
     pub enum Runtime where
         Block = Block,
@@ -1167,8 +1160,57 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllModules,
-    BabeEpochConfigMigrations,
+    (BabeEpochConfigMigrations, FixIndracoreCouncilVotersDeposit),
 >;
+
+pub struct FixIndracoreCouncilVotersDeposit;
+impl frame_support::traits::OnRuntimeUpgrade for FixIndracoreCouncilVotersDeposit {
+	fn on_runtime_upgrade() -> Weight {
+		use pallet_elections_phragmen::Voter;
+		use frame_support::IterableStorageMap;
+		let mut updated = 0;
+		let mut skipped = 0;
+		let mut correct = 0;
+		pallet_elections_phragmen::Voting::<Runtime>::translate::<Voter<AccountId, Balance>, _>(
+			|_who, mut vote| {
+				if vote.deposit == 5 * CENTS {
+					// If their deposit is what we set by mistake
+					vote.deposit = 5 * DOLLARS;
+					updated += 1;
+				} else if vote.deposit == 5 * DOLLARS {
+					correct += 1;
+				} else {
+					skipped += 1;
+				}
+				Some(vote)
+			},
+		);
+
+		log::info!(
+			target: "runtime::indracore",
+			"updated {} (updated) + {} (correct) + {} (skipped) voter's deposit.",
+			updated,
+			correct,
+			skipped,
+		);
+		BlockWeights::get().max_block
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade() -> Result<(), &'static str> {
+		use frame_support::IterableStorageMap;
+		log::info!("Checking FixIndracoreCouncilVotersDeposit post migration");
+		// no further vote with the wrong 5 CENT deposit shall exist.
+		assert!(
+			pallet_elections_phragmen::Voting::<Runtime>::iter().all(
+				|(_, vote)| vote.deposit != 5 * CENTS
+			)
+		);
+
+		Ok(())
+	}
+}
+
 /// The payload being signed in transactions.
 pub type SignedPayload = generic::SignedPayload<Call, SignedExtra>;
 
