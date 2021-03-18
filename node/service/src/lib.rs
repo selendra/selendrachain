7d5636569cfc3233d937bd6730a34d3626864498
laynath242@gmail.com <laynath242@gmail.com>
2021-03-18 09:56:20 +0700

    Reduce number of active leaves at startup

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -38,6 +38,7 @@ use {
     sc_client_api::{AuxStore, ExecutorProvider},
     sc_keystore::LocalKeystore,
     sp_blockchain::HeaderBackend,
+    sp_runtime::traits::Header as HeaderT,
     sp_trie::PrefixedMemoryDB,
     std::convert::TryInto,
     std::time::Duration,
@@ -78,6 +79,9 @@ pub use sp_runtime::traits::{
 
 pub use indracore_runtime;
 pub use kumandra_runtime;
+
+/// The maximum number of active leaves we forward to the [`Overseer`] on startup.
+const MAX_ACTIVE_LEAVES: usize = 4;
 
 native_executor_instance!(
     pub IndracoreExecutor,
@@ -546,6 +550,57 @@ impl IsCollator {
     }
 }
 
+/// Returns the active leaves the overseer should start with.
+#[cfg(feature = "full-node")]
+fn active_leaves<RuntimeApi, Executor>(
+    select_chain: &sc_consensus::LongestChain<FullBackend, Block>,
+    client: &FullClient<RuntimeApi, Executor>,
+) -> Result<Vec<BlockInfo>, Error>
+where
+    RuntimeApi:
+        ConstructRuntimeApi<Block, FullClient<RuntimeApi, Executor>> + Send + Sync + 'static,
+    RuntimeApi::RuntimeApi:
+        RuntimeApiCollection<StateBackend = sc_client_api::StateBackendFor<FullBackend, Block>>,
+    Executor: NativeExecutionDispatch + 'static,
+{
+    let best_block = select_chain.best_chain()?;
+
+    let mut leaves = select_chain
+        .leaves()
+        .unwrap_or_default()
+        .into_iter()
+        .filter_map(|hash| {
+            let number = client.number(hash).ok()??;
+
+            // Only consider leaves that are in maximum an uncle of the best block.
+            if number < best_block.number().saturating_sub(1) {
+                return None;
+            } else if hash == best_block.hash() {
+                return None;
+            };
+
+            let parent_hash = client.header(&BlockId::Hash(hash)).ok()??.parent_hash;
+
+            Some(BlockInfo {
+                hash,
+                parent_hash,
+                number,
+            })
+        })
+        .collect::<Vec<_>>();
+
+    // Sort by block number and get the maximum number of leaves
+    leaves.sort_by_key(|b| b.number);
+
+    leaves.push(BlockInfo {
+        hash: best_block.hash(),
+        parent_hash: *best_block.parent_hash(),
+        number: *best_block.number(),
+    });
+
+    Ok(leaves.into_iter().rev().take(MAX_ACTIVE_LEAVES).collect())
+}
+
 /// Create a new full node of arbitrary runtime and executor.
 ///
 /// This is an advanced feature and not recommended for general use. Generally, `build_full` is
@@ -751,22 +806,7 @@ where
 
     let overseer_client = client.clone();
     let spawner = task_manager.spawn_handle();
-    let leaves: Vec<_> = select_chain
-        .clone()
-        .leaves()
-        .unwrap_or_else(|_| vec![])
-        .into_iter()
-        .filter_map(|hash| {
-            let number = client.number(hash).ok()??;
-            let parent_hash = client.header(&BlockId::Hash(hash)).ok()??.parent_hash;
-
-            Some(BlockInfo {
-                hash,
-                parent_hash,
-                number,
-            })
-        })
-        .collect();
+    let active_leaves = active_leaves(&select_chain, &*client)?;
 
     let authority_discovery_service = if role.is_authority() || is_collator.is_collator() {
         use futures::StreamExt;
@@ -815,7 +855,7 @@ where
 
     let overseer_handler = if let Some((authority_discovery_service, keystore)) = maybe_params {
         let (overseer, overseer_handler) = real_overseer(
-            leaves,
+            active_leaves,
             keystore,
             overseer_client.clone(),
             availability_config,
