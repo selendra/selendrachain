8339d8bfe7913256ae480277eb9ff71d45423e02
laynath242@gmail.com <laynath242@gmail.com>
2021-06-15 09:38:56 +0700

    bump testnet version v3 to v4

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -120,7 +120,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("selendra"),
 	impl_name: create_runtime_str!("selendra-node"),
 	authoring_version: 0,
-	spec_version: 103,
+	spec_version: 104,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
