32d605549fb1491d0fb0d7c3b304c630cf07412d
laynath242@gmail.com <laynath242@gmail.com>
2021-07-12 10:36:44 +0700

    switch branch

--- modules/node/core/dispute-coordinator/src/tests.rs
+++ modules/node/core/dispute-coordinator/src/tests.rs
@@ -53,7 +53,7 @@ struct TestState {
 	validators: Vec<Sr25519Keyring>,
 	validator_public: Vec<ValidatorId>,
 	validator_groups: Vec<Vec<ValidatorIndex>>,
-	polkadot-v0.9.7_keystore: Arc<sc_keystore::LocalKeystore>,
+	polkadot-v0.9.8_keystore: Arc<sc_keystore::LocalKeystore>,
 	subsystem_keystore: Arc<sc_keystore::LocalKeystore>,
 	db: Arc<dyn KeyValueDB>,
 	config: Config,
@@ -80,7 +80,7 @@ impl Default for TestState {
 			vec![ValidatorIndex(4), ValidatorIndex(5)],
 		];
 
-		let polkadot-v0.9.7_keystore = make_keystore(&validators).into();
+		let polkadot-v0.9.8_keystore = make_keystore(&validators).into();
 		let subsystem_keystore = make_keystore(&[Sr25519Keyring::Alice]).into();
 
 		let db = Arc::new(kvdb_memorydb::create(1));
@@ -92,7 +92,7 @@ impl Default for TestState {
 			validators,
 			validator_public,
 			validator_groups,
-			polkadot-v0.9.7_keystore,
+			polkadot-v0.9.8_keystore,
 			subsystem_keystore,
 			db,
 			config,
@@ -196,7 +196,7 @@ impl TestState {
 	) -> SignedDisputeStatement {
 		let public = self.validator_public[index].clone();
 
-		let keystore = self.polkadot-v0.9.7_keystore.clone() as SyncCryptoStorePtr;
+		let keystore = self.polkadot-v0.9.8_keystore.clone() as SyncCryptoStorePtr;
 
 		SignedDisputeStatement::sign_explicit(
 			&keystore,
