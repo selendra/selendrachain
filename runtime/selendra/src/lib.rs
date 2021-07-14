a169d8dfd5b74142ceffc5211918a1724fb2f74f
laynath242@gmail.com <laynath242@gmail.com>
2021-07-14 16:23:54 +0700

    change chain id

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -1266,7 +1266,7 @@ impl FeeCalculator for FixedGasPrice {
 }
 
 parameter_types! {
-	pub const ChainId: u64 = 42;
+	pub const ChainId: u64 = 2000;
 	pub BlockGasLimit: U256 = U256::from(u32::max_value());
 }
 
