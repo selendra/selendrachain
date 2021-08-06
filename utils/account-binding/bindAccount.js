cde6f93278a50bb1ac57b5569e71ea32e75ec8c4
laynath242@gmail.com <laynath242@gmail.com>
2021-08-06 17:42:18 +0700

    update utils:account bindig

--- utils/account-binding/bindAccount.js
+++ utils/account-binding/bindAccount.js
@@ -1,32 +1,62 @@
+const { ArgumentParser } = require('argparse');
+const { ethers} = require('ethers');
 const API = require("@polkadot/api")
 const Web3 = require("web3")
+const version  = require('./package.json');
 const selendraTypes = require('./node-types')
+ 
+const parser = new ArgumentParser({
+  description: 'Selendra evm account binding. Use to bind substrate account to ethereum account'
+});
+ 
+parser.add_argument('-v', '--version', { action: 'version', version });
+parser.add_argument(
+    '-m', '--mnemonic', 
+    {
+        help: 'Account mnemonic, 12 or 24 words'
+    },
+);
 
-const web3 = new Web3("http://localhost:9933");
+parser.add_argument(
+    '-u', '--url',
+    {
+        default: "ws://127.0.0.1:9944",
+        help: 'Websocket provider ,default is http://localhost:9933'
+    }
+);
 
-const PUB_KEY = "0x8C3b33C8cA283e26144e61426326ADc0514D0Fb5"  // Ethereum public key
-const PRIV_KEY = "70cdc2290152ee0afc901d1fd7b6088f43612bb5bf7eb2a12860f3603e52b161" // Ethereum private key
-const SELENDRS_SEEDS = "oppose vintage subject history pilot burden flat wolf pioneer luxury toe coast" // Mnemonic
+parser.add_argument(
+    '-r', '--rpc',
+    {
+        default: "http://localhost:9933",
+        help: 'Selendra rpc ,default is http://localhost:9933'
+    }
+);
 
 async function run() {
-    const wsProvider = new API.WsProvider('ws://127.0.0.1:9944');
+    const wsProvider = new API.WsProvider(parser.parse_args().url);
+    const web3 = new Web3(parser.parse_args().rpc);
+
     const api = await API.ApiPromise.create({
         provider: wsProvider,
         types: selendraTypes
     });
+
     const keyring = new API.Keyring({ type: 'sr25519' });
-    const pioneer = keyring.addFromUri(SELENDRS_SEEDS);
-    let nonce = await api.rpc.system.accountNextIndex(pioneer.address);
-    web3.eth.accounts.wallet.add(PRIV_KEY);
-    let signature = await web3.eth.sign(`Selendra evm:${web3.utils.bytesToHex(pioneer.publicKey).slice(2)}`, PUB_KEY);
+    const substrate_account = keyring.addFromUri(parser.parse_args().mnemonic);
+    let wallet = ethers.Wallet.fromMnemonic(parser.parse_args().mnemonic);
+
+    let nonce = await api.rpc.system.accountNextIndex(substrate_account.address);
+    web3.eth.accounts.wallet.add(wallet.privateKey);
+    let signature = await web3.eth.sign(`Selendra evm:${web3.utils.bytesToHex(substrate_account.publicKey).slice(2)}`, wallet.address);
 
     await api.tx.evmAccounts
-        .claimAccount(PUB_KEY, web3.utils.hexToBytes(signature))
-        .signAndSend(pioneer, {
+        .claimAccount(wallet.address, web3.utils.hexToBytes(signature))
+        .signAndSend(substrate_account, {
             nonce,
         }, ({ events = [], status }) => {
             if (status.isFinalized) {
-                console.log(`${pioneer.address} has bound with EVM address: ${PUB_KEY}`)
+                console.log(`${substrate_account.address} has bound with EVM address: ${wallet.address}`)
                 process.exit(1)
             }
         });
