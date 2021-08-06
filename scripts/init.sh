663efa7db7a217eccaa82568bbe274b3508c1a06
laynath242@gmail.com <laynath242@gmail.com>
2021-08-06 18:52:23 +0700

    config init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,11 +5,12 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
    rustup update stable
+   rustup install nightly-2021-08-02
+   rustup default nightly-2021-08-02
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-08-02
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
