adc3f47f96b4ea36a78832bc627e2adcf848efb2
laynath242@gmail.com <laynath242@gmail.com>
2021-08-11 15:25:19 +0700

    update

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -16,21 +16,21 @@
 
 //! Selendra chain configurations.
 
-use beefy_primitives::crypto::AuthorityId as BeefyId;
-use grandpa::AuthorityId as GrandpaId;
-use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
-use selendra_primitives::v1::{AccountId, AccountPublic, AssignmentId, ValidatorId};
-use selendra_runtime as selendra;
-use selendra_runtime::constants::currency::UNITS as SEL;
 use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
 use sp_consensus_babe::AuthorityId as BabeId;
+use beefy_primitives::crypto::AuthorityId as BeefyId;
+use grandpa::AuthorityId as GrandpaId;
+use selendra_runtime as selendra;
+use selendra_runtime::constants::currency::UNITS as SEL;
+use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
+use selendra_primitives::v1::{AccountId, AccountPublic, AssignmentId, ValidatorId};
 
 use sc_chain_spec::{ChainSpecExtension, ChainType};
 use serde::{Deserialize, Serialize};
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::{traits::IdentifyAccount, Perbill};
-use std::collections::BTreeMap;
 use telemetry::TelemetryEndpoints;
+use std::collections::BTreeMap;
 
 const SELENDRA_STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 const DEFAULT_PROTOCOL_ID: &str = "sel";
@@ -42,84 +42,83 @@ const DEFAULT_PROTOCOL_ID: &str = "sel";
 #[derive(Default, Clone, Serialize, Deserialize, ChainSpecExtension)]
 #[serde(rename_all = "camelCase")]
 pub struct Extensions {
-    /// Block numbers with known hashes.
-    pub fork_blocks: sc_client_api::ForkBlocks<selendra_primitives::v1::Block>,
-    /// Known bad block hashes.
-    pub bad_blocks: sc_client_api::BadBlocks<selendra_primitives::v1::Block>,
+	/// Block numbers with known hashes.
+	pub fork_blocks: sc_client_api::ForkBlocks<selendra_primitives::v1::Block>,
+	/// Known bad block hashes.
+	pub bad_blocks: sc_client_api::BadBlocks<selendra_primitives::v1::Block>,
 }
 
 /// The `ChainSpec` parameterized for the selendra runtime.
 pub type SelendraChainSpec = service::GenericChainSpec<selendra::GenesisConfig, Extensions>;
 
 pub fn selendra_config() -> Result<SelendraChainSpec, String> {
-    SelendraChainSpec::from_json_bytes(&include_bytes!("../res/selendra.json")[..])
+	SelendraChainSpec::from_json_bytes(&include_bytes!("../res/selendra.json")[..])
 }
 
 /// The default parachains host configuration.
-fn default_parachains_host_configuration(
-) -> selendra_runtime_parachains::configuration::HostConfiguration<
-    selendra_primitives::v1::BlockNumber,
-> {
-    use selendra_primitives::v1::{MAX_CODE_SIZE, MAX_POV_SIZE};
+fn default_parachains_host_configuration() ->
+	selendra_runtime_parachains::configuration::HostConfiguration<selendra_primitives::v1::BlockNumber>
+{
+	use selendra_primitives::v1::{MAX_CODE_SIZE, MAX_POV_SIZE};
 
-    selendra_runtime_parachains::configuration::HostConfiguration {
-        validation_upgrade_frequency: 1u32,
-        validation_upgrade_delay: 1,
-        code_retention_period: 1200,
-        max_code_size: MAX_CODE_SIZE,
-        max_pov_size: MAX_POV_SIZE,
-        max_head_data_size: 32 * 1024,
-        group_rotation_frequency: 20,
-        chain_availability_period: 4,
-        thread_availability_period: 4,
-        max_upward_queue_count: 8,
-        max_upward_queue_size: 1024 * 1024,
-        max_downward_message_size: 1024,
-        // this is approximatelly 4ms.
-        //
-        // Same as `4 * frame_support::weights::WEIGHT_PER_MILLIS`. We don't bother with
-        // an import since that's a made up number and should be replaced with a constant
-        // obtained by benchmarking anyway.
-        ump_service_total_weight: 4 * 1_000_000_000,
-        max_upward_message_size: 1024 * 1024,
-        max_upward_message_num_per_candidate: 5,
-        hrmp_open_request_ttl: 5,
-        hrmp_sender_deposit: 0,
-        hrmp_recipient_deposit: 0,
-        hrmp_channel_max_capacity: 8,
-        hrmp_channel_max_total_size: 8 * 1024,
-        hrmp_max_parachain_inbound_channels: 4,
-        hrmp_max_parathread_inbound_channels: 4,
-        hrmp_channel_max_message_size: 1024 * 1024,
-        hrmp_max_parachain_outbound_channels: 4,
-        hrmp_max_parathread_outbound_channels: 4,
-        hrmp_max_message_num_per_candidate: 5,
-        dispute_period: 6,
-        no_show_slots: 2,
-        n_delay_tranches: 25,
-        needed_approvals: 2,
-        relay_vrf_modulo_samples: 2,
-        zeroth_delay_tranche_width: 0,
-        ..Default::default()
-    }
+	selendra_runtime_parachains::configuration::HostConfiguration {
+		validation_upgrade_frequency: 1u32,
+		validation_upgrade_delay: 1,
+		code_retention_period: 1200,
+		max_code_size: MAX_CODE_SIZE,
+		max_pov_size: MAX_POV_SIZE,
+		max_head_data_size: 32 * 1024,
+		group_rotation_frequency: 20,
+		chain_availability_period: 4,
+		thread_availability_period: 4,
+		max_upward_queue_count: 8,
+		max_upward_queue_size: 1024 * 1024,
+		max_downward_message_size: 1024,
+		// this is approximatelly 4ms.
+		//
+		// Same as `4 * frame_support::weights::WEIGHT_PER_MILLIS`. We don't bother with
+		// an import since that's a made up number and should be replaced with a constant
+		// obtained by benchmarking anyway.
+		ump_service_total_weight: 4 * 1_000_000_000,
+		max_upward_message_size: 1024 * 1024,
+		max_upward_message_num_per_candidate: 5,
+		hrmp_open_request_ttl: 5,
+		hrmp_sender_deposit: 0,
+		hrmp_recipient_deposit: 0,
+		hrmp_channel_max_capacity: 8,
+		hrmp_channel_max_total_size: 8 * 1024,
+		hrmp_max_parachain_inbound_channels: 4,
+		hrmp_max_parathread_inbound_channels: 4,
+		hrmp_channel_max_message_size: 1024 * 1024,
+		hrmp_max_parachain_outbound_channels: 4,
+		hrmp_max_parathread_outbound_channels: 4,
+		hrmp_max_message_num_per_candidate: 5,
+		dispute_period: 6,
+		no_show_slots: 2,
+		n_delay_tranches: 25,
+		needed_approvals: 2,
+		relay_vrf_modulo_samples: 2,
+		zeroth_delay_tranche_width: 0,
+		..Default::default()
+	}
 }
 
 fn selendra_session_keys(
-    babe: BabeId,
-    grandpa: GrandpaId,
-    im_online: ImOnlineId,
-    para_validator: ValidatorId,
-    para_assignment: AssignmentId,
-    authority_discovery: AuthorityDiscoveryId,
+	babe: BabeId,
+	grandpa: GrandpaId,
+	im_online: ImOnlineId,
+	para_validator: ValidatorId,
+	para_assignment: AssignmentId,
+	authority_discovery: AuthorityDiscoveryId,
 ) -> selendra::SessionKeys {
-    selendra::SessionKeys {
-        babe,
-        grandpa,
-        im_online,
-        para_validator,
-        para_assignment,
-        authority_discovery,
-    }
+	selendra::SessionKeys {
+		babe,
+		grandpa,
+		im_online,
+		para_validator,
+		para_assignment,
+		authority_discovery,
+	}
 }
 
 fn selendra_staging_testnet_config_genesis(wasm_binary: &[u8]) -> selendra::GenesisConfig {
@@ -282,23 +281,24 @@ fn selendra_staging_testnet_config_genesis(wasm_binary: &[u8]) -> selendra::Gene
 	}
 }
 
+
 /// Staging testnet config.
 pub fn selendra_staging_testnet_config() -> Result<SelendraChainSpec, String> {
-    let wasm_binary = selendra::WASM_BINARY.ok_or("Selendra development wasm not available")?;
-    let boot_nodes = vec![];
+	let wasm_binary = selendra::WASM_BINARY.ok_or("Selendra development wasm not available")?;
+	let boot_nodes = vec![];
 
-    Ok(SelendraChainSpec::from_genesis(
-        "Selendra Staging Testnet",
-        "selendra_staging_testnet",
-        ChainType::Live,
-        move || selendra_staging_testnet_config_genesis(wasm_binary),
-        boot_nodes,
-        Some(
-            TelemetryEndpoints::new(vec![(SELENDRA_STAGING_TELEMETRY_URL.to_string(), 0)])
-                .expect("Selendra Staging telemetry url is valid; qed"),
-        ),
-        Some(DEFAULT_PROTOCOL_ID),
-        Some(
+	Ok(SelendraChainSpec::from_genesis(
+		"Selendra Staging Testnet",
+		"selendra_staging_testnet",
+		ChainType::Live,
+		move || selendra_staging_testnet_config_genesis(wasm_binary),
+		boot_nodes,
+		Some(
+			TelemetryEndpoints::new(vec![(SELENDRA_STAGING_TELEMETRY_URL.to_string(), 0)])
+				.expect("Selendra Staging telemetry url is valid; qed"),
+		),
+		Some(DEFAULT_PROTOCOL_ID),
+		Some(
             serde_json::from_str(
                 "{
             \"tokenDecimals\": 18,
@@ -307,110 +307,103 @@ pub fn selendra_staging_testnet_config() -> Result<SelendraChainSpec, String> {
             )
             .expect("Provided valid json map"),
         ),
-        Default::default(),
-    ))
+		Default::default(),
+	))
 }
 
 /// Helper function to generate a crypto pair from seed
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-    TPublic::Pair::from_string(&format!("//{}", seed), None)
-        .expect("static values are valid; qed")
-        .public()
+	TPublic::Pair::from_string(&format!("//{}", seed), None)
+		.expect("static values are valid; qed")
+		.public()
 }
 
 /// Helper function to generate an account ID from seed
 pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
 where
-    AccountPublic: From<<TPublic::Pair as Pair>::Public>,
+	AccountPublic: From<<TPublic::Pair as Pair>::Public>,
 {
-    AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
 /// Helper function to generate stash, controller and session key from seed
 pub fn get_authority_keys_from_seed(
-    seed: &str,
+	seed: &str,
 ) -> (
-    AccountId,
-    AccountId,
-    BabeId,
-    GrandpaId,
-    ImOnlineId,
-    ValidatorId,
-    AssignmentId,
-    AuthorityDiscoveryId,
-    BeefyId,
+	AccountId,
+	AccountId,
+	BabeId,
+	GrandpaId,
+	ImOnlineId,
+	ValidatorId,
+	AssignmentId,
+	AuthorityDiscoveryId,
+	BeefyId,
 ) {
-    let keys = get_authority_keys_from_seed_no_beefy(seed);
-    (
-        keys.0,
-        keys.1,
-        keys.2,
-        keys.3,
-        keys.4,
-        keys.5,
-        keys.6,
-        keys.7,
-        get_from_seed::<BeefyId>(seed),
-    )
+	let keys = get_authority_keys_from_seed_no_beefy(seed);
+	(
+		keys.0, keys.1, keys.2, keys.3, keys.4, keys.5, keys.6, keys.7, get_from_seed::<BeefyId>(seed)
+	)
 }
 
 /// Helper function to generate stash, controller and session key from seed
 pub fn get_authority_keys_from_seed_no_beefy(
-    seed: &str,
+	seed: &str,
 ) -> (
-    AccountId,
-    AccountId,
-    BabeId,
-    GrandpaId,
-    ImOnlineId,
-    ValidatorId,
-    AssignmentId,
-    AuthorityDiscoveryId,
+	AccountId,
+	AccountId,
+	BabeId,
+	GrandpaId,
+	ImOnlineId,
+	ValidatorId,
+	AssignmentId,
+	AuthorityDiscoveryId,
 ) {
-    (
-        get_account_id_from_seed::<sr25519::Public>(&format!("{}//stash", seed)),
-        get_account_id_from_seed::<sr25519::Public>(seed),
-        get_from_seed::<BabeId>(seed),
-        get_from_seed::<GrandpaId>(seed),
-        get_from_seed::<ImOnlineId>(seed),
-        get_from_seed::<ValidatorId>(seed),
-        get_from_seed::<AssignmentId>(seed),
-        get_from_seed::<AuthorityDiscoveryId>(seed),
-    )
+	(
+		get_account_id_from_seed::<sr25519::Public>(&format!("{}//stash", seed)),
+		get_account_id_from_seed::<sr25519::Public>(seed),
+		get_from_seed::<BabeId>(seed),
+		get_from_seed::<GrandpaId>(seed),
+		get_from_seed::<ImOnlineId>(seed),
+		get_from_seed::<ValidatorId>(seed),
+		get_from_seed::<AssignmentId>(seed),
+		get_from_seed::<AuthorityDiscoveryId>(seed),
+	)
 }
 
 fn testnet_accounts() -> Vec<AccountId> {
-    vec![
-        get_account_id_from_seed::<sr25519::Public>("Alice"),
-        get_account_id_from_seed::<sr25519::Public>("Bob"),
-        get_account_id_from_seed::<sr25519::Public>("Charlie"),
-        get_account_id_from_seed::<sr25519::Public>("Dave"),
-        get_account_id_from_seed::<sr25519::Public>("Eve"),
-        get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-        get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-    ]
+	vec![
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		get_account_id_from_seed::<sr25519::Public>("Bob"),
+		get_account_id_from_seed::<sr25519::Public>("Charlie"),
+		get_account_id_from_seed::<sr25519::Public>("Dave"),
+		get_account_id_from_seed::<sr25519::Public>("Eve"),
+		get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+		get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+		get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+		get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+		get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+		get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+		get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+	]
 }
+
 
 /// Helper function to create selendra GenesisConfig for testing
 pub fn selendra_testnet_genesis(
-    wasm_binary: &[u8],
-    initial_authorities: Vec<(
-        AccountId,
-        AccountId,
-        BabeId,
-        GrandpaId,
-        ImOnlineId,
-        ValidatorId,
-        AssignmentId,
-        AuthorityDiscoveryId,
-    )>,
-    _root_key: AccountId,
-    endowed_accounts: Option<Vec<AccountId>>,
+	wasm_binary: &[u8],
+	initial_authorities: Vec<(
+		AccountId,
+		AccountId,
+		BabeId,
+		GrandpaId,
+		ImOnlineId,
+		ValidatorId,
+		AssignmentId,
+		AuthorityDiscoveryId,
+	)>,
+	_root_key: AccountId,
+	endowed_accounts: Option<Vec<AccountId>>,
 ) -> selendra::GenesisConfig {
 	let endowed_accounts: Vec<AccountId> = endowed_accounts.unwrap_or_else(testnet_accounts);
 
@@ -504,27 +497,27 @@ pub fn selendra_testnet_genesis(
 }
 
 fn selendra_development_config_genesis(wasm_binary: &[u8]) -> selendra::GenesisConfig {
-    selendra_testnet_genesis(
-        wasm_binary,
-        vec![get_authority_keys_from_seed_no_beefy("Alice")],
-        get_account_id_from_seed::<sr25519::Public>("Alice"),
-        None,
-    )
+	selendra_testnet_genesis(
+		wasm_binary,
+		vec![get_authority_keys_from_seed_no_beefy("Alice")],
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		None,
+	)
 }
 
 /// Selendra development config (single validator Alice)
 pub fn selendra_development_config() -> Result<SelendraChainSpec, String> {
-    let wasm_binary = selendra::WASM_BINARY.ok_or("Selendra development wasm not available")?;
+	let wasm_binary = selendra::WASM_BINARY.ok_or("Selendra development wasm not available")?;
 
-    Ok(SelendraChainSpec::from_genesis(
-        "Development",
-        "selendra_dev",
-        ChainType::Development,
-        move || selendra_development_config_genesis(wasm_binary),
-        vec![],
-        None,
-        Some(DEFAULT_PROTOCOL_ID),
-        Some(
+	Ok(SelendraChainSpec::from_genesis(
+		"Development",
+		"selendra_dev",
+		ChainType::Development,
+		move || selendra_development_config_genesis(wasm_binary),
+		vec![],
+		None,
+		Some(DEFAULT_PROTOCOL_ID),
+		Some(
             serde_json::from_str(
                 "{
             \"tokenDecimals\": 18,
@@ -533,35 +526,35 @@ pub fn selendra_development_config() -> Result<SelendraChainSpec, String> {
             )
             .expect("Provided valid json map"),
         ),
-        Default::default(),
-    ))
+		Default::default(),
+	))
 }
 
 fn selendra_local_testnet_genesis(wasm_binary: &[u8]) -> selendra::GenesisConfig {
-    selendra_testnet_genesis(
-        wasm_binary,
-        vec![
-            get_authority_keys_from_seed_no_beefy("Alice"),
-            get_authority_keys_from_seed_no_beefy("Bob"),
-        ],
-        get_account_id_from_seed::<sr25519::Public>("Alice"),
-        None,
-    )
+	selendra_testnet_genesis(
+		wasm_binary,
+		vec![
+			get_authority_keys_from_seed_no_beefy("Alice"),
+			get_authority_keys_from_seed_no_beefy("Bob"),
+		],
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		None,
+	)
 }
 
 /// Selendra local testnet config (multivalidator Alice + Bob)
 pub fn selendra_local_testnet_config() -> Result<SelendraChainSpec, String> {
-    let wasm_binary = selendra::WASM_BINARY.ok_or("Selendra development wasm not available")?;
+	let wasm_binary = selendra::WASM_BINARY.ok_or("Selendra development wasm not available")?;
 
-    Ok(SelendraChainSpec::from_genesis(
-        "Selendra Local Testnet",
-        "selendra_local_testnet",
-        ChainType::Local,
-        move || selendra_local_testnet_genesis(wasm_binary),
-        vec![],
-        None,
-        Some(DEFAULT_PROTOCOL_ID),
-        Some(
+	Ok(SelendraChainSpec::from_genesis(
+		"Selendra Local Testnet",
+		"selendra_local_testnet",
+		ChainType::Local,
+		move || selendra_local_testnet_genesis(wasm_binary),
+		vec![],
+		None,
+		Some(DEFAULT_PROTOCOL_ID),
+		Some(
             serde_json::from_str(
                 "{
             \"tokenDecimals\": 18,
@@ -570,6 +563,6 @@ pub fn selendra_local_testnet_config() -> Result<SelendraChainSpec, String> {
             )
             .expect("Provided valid json map"),
         ),
-        Default::default(),
-    ))
+		Default::default(),
+	))
 }
