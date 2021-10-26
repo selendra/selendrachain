0f62bbd9f48385cf3552d3378a2faabf173ff294
LayNath242 <laynath243@gmail.com>
2021-10-26 10:50:53 +0700

    rename

--- scripts/create_service.sh
+++ scripts/create_service.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 {
     echo '[Unit]'
-    echo 'Description=Indracore'
+    echo 'Description=Selendra'
     echo '[Service]'
     echo 'Type=exec'
     echo 'WorkingDirectory='`pwd`
-    echo 'ExecStart='`pwd`'/target/release/selendra --chain=sel --ws-external --rpc-cors "*"'
+    echo 'ExecStart='`pwd`'/target/release/selendra --chain=local --ws-external --rpc-cors "*"'
     echo '[Install]'
     echo 'WantedBy=multi-user.target'
 } > /etc/systemd/system/indracore.service
