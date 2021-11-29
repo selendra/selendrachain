2b24eedec1edd4316adf82988caff4a1bf9de719
LayNath242 <laynath243@gmail.com>
2021-11-29 08:05:28 +0700

    rollback selendra transaction

--- modules/bridge_transfer/src/tests.rs
+++ modules/bridge_transfer/src/tests.rs
@@ -3,9 +3,9 @@
 use super::{
 	bridge,
 	mock::{
-		assert_events, expect_event, new_test_ext, Balances, Bridge, BridgeTransfer, Call, Event,
-		NativeTokenResourceId, Origin, ProposalLifetime, Test, ENDOWED_BALANCE, RELAYER_A,
-		RELAYER_B, RELAYER_C,
+		assert_events, expect_event, new_test_ext, Balances, Bridge, BridgeTransfer,
+		Call, Event, NativeTokenResourceId, Origin, ProposalLifetime, Test, ENDOWED_BALANCE,
+		RELAYER_A, RELAYER_B, RELAYER_C,
 	},
 	*,
 };
