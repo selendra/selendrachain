1feda2180614653f05b7db9c48338136653c954d
LayNath242 <laynath243@gmail.com>
2021-12-03 13:22:02 +0700

    bump selendra to v0.1.6

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("selendra"),
 	impl_name: create_runtime_str!("selendra-chain"),
 	authoring_version: 1,
-	spec_version: 114,
+	spec_version: 116,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
