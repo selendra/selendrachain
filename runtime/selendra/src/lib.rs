1ad13a99c47288eeb1b8293a865accfc0d8e8047
LayNath242 <laynath242@gmail.com>
2021-12-08 10:16:10 +0700

    update inflation

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -464,8 +464,8 @@ impl pallet_bags_list::Config for Runtime {
 
 pallet_staking_reward_curve::build! {
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-		min_inflation: 0_025_000,
-		max_inflation: 0_100_000,
+		min_inflation: 0_034_500,
+		max_inflation: 0_044_500,
 		ideal_stake: 0_500_000,
 		falloff: 0_050_000,
 		max_piece_count: 40,
