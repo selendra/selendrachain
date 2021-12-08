d317704691ff854b1e0c9773356ff1f5343e8e1f
LayNath242 <laynath242@gmail.com>
2021-12-08 09:00:36 +0700

    bump selendra-chain

--- node/network/availability-distribution/src/requester/fetch_task/mod.rs
+++ node/network/availability-distribution/src/requester/fetch_task/mod.rs
@@ -13,6 +13,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+#![allow(unused_must_use)]
 
 use std::collections::HashSet;
 
