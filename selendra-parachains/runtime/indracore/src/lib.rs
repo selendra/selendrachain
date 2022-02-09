600c05ff74e4eb80b390c7391a7559a56f58ffcf
LayNath242 <laynath242@gmail.com>
2022-02-09 11:39:03 +0700

    update script

--- selendra-parachains/runtime/indracore/src/lib.rs
+++ selendra-parachains/runtime/indracore/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("indracore"),
 	impl_name: create_runtime_str!("indracore"),
 	authoring_version: 1,
-	spec_version: 601,
+	spec_version: 1,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
